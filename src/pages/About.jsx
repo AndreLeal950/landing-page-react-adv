ea093a87812fb196e8e638c28c5c90f005ac256b
AndreLeal950 <lealbelem@yahoo.com.br>
2023-10-04 15:21:23 -0300

    Confeclção da pagina sobre.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,78 @@
+import "./About.css";
+
 const About = () => {
 	return (
-		<div>
-			Página Sobre
-			<p>
-				<a href="/">voltar para home</a>
-			</p>
+		<div className="container">
+			<div className="container-text">
+				<h1 className="title">Sobre [Nome do Escritório ]</h1>
+				<h3>Bem-vindo ao [Nome do Escritório] :</h3>
+				<p>
+					Bem-vindo ao [Nome do Escritório de Advocacia], onde a experiência
+					jurídica encontra soluções eficazes. Somos um escritório de advocacia
+					dedicado a fornecer orientação legal especializada em direito
+					empresarial e direito do trabalho para empresas e empregadores.
+				</p>
+				<h3>Nossa Missão:</h3>
+				<p>
+					Nosso compromisso é oferecer suporte jurídico excepcional, ajudando
+					nossos clientes a navegar pelas complexidades legais do mundo
+					empresarial e das relações de trabalho. Estamos empenhados em proteger
+					seus interesses, promovendo práticas justas e ajudando a construir
+					ambientes de trabalho saudáveis.
+				</p>
+				<h3>Nossos Valores :</h3>
+				<p>
+					<span>
+						Nosso trabalho é guiado por valores fundamentais que refletem nossa
+						abordagem :
+					</span>
+					<br /> <br />
+					<span>Excelência :</span> Buscamos a excelência em tudo o que fazemos,
+					garantindo a mais alta qualidade de serviços jurídicos para nossos
+					clientes.
+					<br />
+					<span>Integridade : </span> Atuamos com integridade e ética em todas
+					as interações, mantendo a confiança dos clientes como nossa
+					prioridade.
+					<br />
+					<span>Empatia : </span>Compreendemos as necessidades e desafios de
+					nossos clientes, e nossa abordagem é sempre orientada para servir e
+					apoiar.
+					<br />
+					<span>Inovação :</span> Abraçamos soluções inovadoras e estratégias
+					jurídicas criativas para alcançar resultados eficazes.
+				</p>
+				<h3>Nossos Serviços : </h3>
+				<p>
+					<span>Direito Empresarial : </span> Oferecemos consultoria jurídica
+					abrangente para empresas em todas as etapas, desde a formação até
+					transações complexas de fusões e aquisições. Nossa equipe
+					especializada em direito societário, contratos comerciais e
+					propriedade intelectual está à disposição para atender às suas
+					necessidades. <br />
+					<span>Direito do Trabalho : </span>
+					Protegemos os interesses dos empregadores, fornecendo assistência em
+					contratações, resolução de conflitos trabalhistas, conformidade
+					regulatória, mediação e treinamento em conformidade. Estamos ao seu
+					lado para enfrentar desafios relacionados a recursos humanos.
+				</p>
+				<h3>Nossa Abordagem : </h3>
+				<p>
+					No [Nome do Escritório de Advocacia], valorizamos relacionamentos
+					sólidos com nossos clientes. Entendemos as particularidades de seus
+					negócios e trabalhamos de forma estratégica para alcançar soluções
+					eficazes. Nossa equipe de advogados experientes está comprometida com
+					a excelência, integridade e respeito pelo cliente.
+				</p>
+
+				<a href="/">
+					<input
+						type="submit"
+						value="voltar para a página incial"
+						className="btn  button"
+					/>
+				</a>
+			</div>
 		</div>
 	);
 };
