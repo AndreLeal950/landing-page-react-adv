f31537f7e9ee8af1ebc312160de93cacddf96ec9
AndreLeal950 <lealbelem@yahoo.com.br>
2023-10-04 14:14:32 -0300

    configuração da pagina de serviços.

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,10 +1,67 @@
+import "./Services.css";
+
 function Services() {
 	return (
-		<div>
-			Pagina de Serviços
-			<p>
-				<a href="/">voltar para home</a>
-			</p>
+		<div className="container">
+			<div className="container-text">
+				<h1 className="title">
+					Alguns dos Serviços oferecidos pelo nosso Escritório
+				</h1>
+				<h3>Direito Empresarial:</h3>
+				<p>
+					1. Consultoria Empresarial: Orientação legal geral para empresas,
+					incluindo questões contratuais, formação de empresas e estratégias de
+					negócios. <br />
+					2. Fusões e Aquisições (M&A): Assistência em transações de compra e
+					venda de empresas, fusões, aquisições e due diligence. <br />
+					3. Propriedade Intelectual: Proteção de ativos de propriedade
+					intelectual, como marcas, patentes e direitos autorais.
+					<br />
+					4. Conformidade Regulatória: Garantir que a empresa esteja em
+					conformidade com todas as leis e regulamentos aplicáveis ao seu setor.
+					<br />
+					5. Contratos Comerciais: Elaboração e revisão de contratos comerciais,
+					acordos de parceria e contratos de fornecimento.
+					<br />
+					6. Resolução de Conflitos Empresariais: Representação em litígios
+					empresariais, incluindo disputas contratuais e comerciais.
+					<br />
+					7. Direito Societário: Assessoria na formação de empresas,
+					reestruturações societárias e governança corporativa.
+				</p>
+				<h3>Direito do Trabalho:</h3>
+				<p>
+					1. Contratação e Rescisão de Funcionários: Elaboração de contratos de
+					trabalho, políticas de recursos humanos e procedimentos de rescisão.
+					<br />
+					2. Litígios Trabalhistas: Representação de empregadores em casos de
+					litígios, como demissões injustas, discriminação e assédio. <br />
+					3. Negociação Coletiva: Negociação de contratos coletivos com
+					sindicatos e gestão de disputas trabalhistas.
+					<br />
+					4. Conformidade Trabalhista: Assegurar que a empresa cumpra as leis
+					trabalhistas, regulamentos e normas ocupacionais.
+					<br />
+					5. Mediação e Arbitragem: Resolver disputas entre empregadores e
+					funcionários por meio de métodos alternativos, evitando litígios
+					judiciais. <br />
+					6. Auditoria Trabalhista: Realizar auditorias internas para garantir a
+					conformidade com regulamentos trabalhistas. <br />
+					7. Treinamento em Conformidade: Oferecer treinamento para funcionários
+					e empregadores sobre questões trabalhistas, como assédio e
+					discriminação. <br />
+					8. Segurança Ocupacional: Assessoria em questões relacionadas à
+					segurança no local de trabalho e regulamentos de saúde ocupacional.
+				</p>
+
+				<a href="/">
+					<input
+						type="submit"
+						value="voltar para a página incial"
+						className="btn  button"
+					/>
+				</a>
+			</div>
 		</div>
 	);
 }
